03566544a20f16f3610fa4ca0253e37de08e25a7
Julie Perilla <julie.perilla@gmail.com>
2013-05-19 16:25:43 -0800

    making bookmarklet look nice

--- Web/Scripts/freelance-hub-bookmarklet.js
+++ Web/Scripts/freelance-hub-bookmarklet.js
@@ -36,7 +36,7 @@
 						<style type='text/css'>\
 							#fh_frame_veil { display: none; position: fixed; width: 100%; height: 100%; top: 0; left: 0; background-color: rgba(255,255,255,.25); cursor: pointer; z-index: 900; }\
 							#fh_frame_veil p { color: black; font: normal normal bold 20px/20px Helvetica, sans-serif; position: absolute; top: 50%; left: 50%; width: 10em; margin: -10px auto 0 -5em; text-align: center; }\
-							#fh_frame iframe { display: none; position: fixed; top: 10%; left: 10%; width: 80%; height: 80%; z-index: 999; border: 10px solid rgba(0,0,0,.5); margin: -5px 0 0 -5px; }\
+							#fh_frame iframe { display: none; position: fixed; top: 10%; left: 10%; width: 30%; height: 80%; z-index: 999; border: 10px solid rgba(0,0,0,.5); margin: -5px 0 0 -5px; }\
 						</style>\
 					</div>");
                     $("#fh_frame_veil").fadeIn(750);
